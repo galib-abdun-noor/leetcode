d0ecffe6f153ba3e7fcf916c24ff3c4dd451cdac
Galib Abdun Noor <67048010+galib-abdun-noor@users.noreply.github.com>
2023-04-17 20:24:45 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -197,6 +197,10 @@ public class Main {
         System.out.println("output: "+stn.isBST(root2));
         System.out.println("Excel Sheet Column title");
         System.out.println("output: "+stn.numberToTitle(701));
+        int[] nums7 = {1,2};
+        int[] nums8 = {3,4};
+        System.out.println("4. Median of Two Sorted Arrays");
+        System.out.println("output: "+stn.findMedianSortedArrays(nums7,nums8));
     }
 }
 
@@ -1002,6 +1006,28 @@ class Solution{
             title = val + title;
         }
         return title;
+    }
+    
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int length = nums1.length+nums2.length;
+        int[] arr = new int[length];
+        int i=0,j=0,k=0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]<nums2[j]){
+                arr[k]=nums1[i];
+                i++;
+            }else{
+                arr[k]=nums2[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<nums1.length || j<nums2.length){
+            arr[k] = i<nums1.length?(arr[k]=nums1[i++]):(arr[k]=nums2[j++]);
+            k++;
+        }
+        double med = (length%2==0)?((Double.valueOf(arr[length/2-1])+Double.valueOf(arr[length/2]))/2):arr[length/2];
+        return med;
     }
 
 }
